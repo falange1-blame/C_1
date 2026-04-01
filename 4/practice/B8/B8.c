5c0591a381ad02c45757390e04f670574ae6a4b8
Yang Yeon Wo <yangyeonwoo423@gmail.com>
2026-04-01 21:22:22 +0900

    week_5_practice

--- 4/practice/B8/B8.c
+++ 4/practice/B8/B8.c
@@ -1,31 +1,90 @@
 //Embedded Register Simulator
-#define POWER   0x01
-#define READY   0x03
-#define SENSOR  0x07
-#define MOTOR   0x0F
-
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+
+#define POWER   0x01
+#define READY   0x02
+#define SENSOR  0x04
+#define MOTOR   0x08
+#define ERROR   0x80
+
 void print_binary_8(unsigned char val)
 {
 	int i;
 	for (i = 7; i >= 0; i--)
 	{
-		printf("%d", (i >> val) & 1);
+		printf("%d", (val >> i) & 1);
 	}
 	printf("\n");
 }
 
 int main(void)
 {
-	unsigned char Initial = 0x00;
+	unsigned char reg = 0x00;
 	
-	printf("Initial:          0x%02x  ", Initial);
-	print_binary_8(Initial);
+	printf("=== Embedded Register Simulator===\n");
 
-	printf("SET POWER:        0x%02x  ", POWER);
-	print_binary_8(POWER);
+	printf("Initial:              0x%02X  ", reg);
+	print_binary_8(reg);
 
+	reg |= POWER;
+	printf("SET POWER:            0x%02X  ", reg);
+	print_binary_8(reg);
 
+	reg |= READY;
+	printf("SET READY:            0x%02X  ", reg);
+	print_binary_8(reg);
 
+	reg |= SENSOR;
+	printf("SET SENSOR:           0x%02X  ", reg);
+	print_binary_8(reg);
+
+	reg |= MOTOR;
+	printf("SET MOTOR:            0x%02X  ", reg);
+	print_binary_8(reg);
+	printf("\n\n");
+	
+	if (reg &= MOTOR)
+	{
+		printf("Motor is ON\n\n");
+	}
+
+	else
+	{
+		printf("Motor is OFF\n\n");
+	}
+
+	reg ^= SENSOR;
+	printf("CLEAR SENSOR:         0x%02X  ", reg);
+	print_binary_8(reg);
+	
+	
+	reg ^= MOTOR;
+	printf("TOGGLE MOTOR:         0x%02X  ", reg);
+	print_binary_8(reg);
+
+	reg ^= MOTOR;
+	printf("TOGGLE MOTOR:         0x%02X  ", reg);
+	print_binary_8(reg);
+
+	reg |= ERROR;
+	
+	if (reg &= MOTOR)
+	{
+		printf("Motor is ON\n");
+	}
+	else
+	{
+		printf("Motor is OFF\n");
+	}
+
+	if (reg &= ERROR)
+	{
+		printf("Error is SET\n");
+	}
+	else
+	{
+		printf("Error is NOT SET\n");
+	}
+
+	return 0;
 }
